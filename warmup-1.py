8c8dc6c6557c60582dc411336eb5dfb5b9083a9d
edkrueger <edkrueger@gmail.com>
2023-09-05 16:36:02 -0500

    solve sum_double

--- warmup-1.py
+++ warmup-1.py
@@ -34,7 +34,12 @@ def sum_double(a, b):
     sum_double(3, 2) → 5
     sum_double(2, 2) → 8
     """
-    return
+
+    # if a == b:
+    #     return 2 * (a + b)
+    # return a + b
+
+    return 2 * (a + b) if a == b else a + b
 
 
 def diff21(n):
